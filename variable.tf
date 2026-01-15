e4af3e466259773093121ac95c075efb3c218584
standinleo <ne.minhtri@gmail.com>
2026-01-15 06:53:54 +0000

    Add new file

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "ap-southeast-1"
+}
+
+variable "app_name" {
+  default = "sample-eb-app"
+}
+
+variable "env_name" {
+  default = "sample-eb-env"
+}
+
+variable "solution_stack" {
+  default = "64bit Amazon Linux 2023 v6.0.2 running Node.js 18"
+}
