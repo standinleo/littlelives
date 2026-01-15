a7b5f8fc2e45c1bdb4f17442ae6e47f0dcab3ceb
standinleo <ne.minhtri@gmail.com>
2026-01-15 07:21:26 +0000

    Edit main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,33 @@
 provider "aws" {
   region = var.region
 }
-resource "aws_s3_bucket" "eb_bucket" {
-  bucket = "${var.app_name}-bucket-${random_id.suffix.hex}"
-}
 
 resource "random_id" "suffix" {
   byte_length = 4
 }
-resource "aws_s3_object" "app_zip" {
-  bucket = aws_s3_bucket.eb_bucket.id
-  key    = "app.zip"
-  source = "app/app.zip"
+
+resource "aws_s3_bucket" "eb_bucket" {
+  bucket = "${var.app_name}-eb-${random_id.suffix.hex}"
 }
+
 resource "aws_elastic_beanstalk_application" "app" {
-  name        = var.app_name
-  description = "Elastic Beanstalk app deployed with Terraform"
+  name = var.app_name
 }
-resource "aws_elastic_beanstalk_application_version" "version" {
-  name        = "v1"
-  application = aws_elastic_beanstalk_application.app.name
-  bucket      = aws_s3_bucket.eb_bucket.id
-  key         = aws_s3_object.app_zip.key
-}
+
 resource "aws_elastic_beanstalk_environment" "env" {
   name                = var.env_name
   application         = aws_elastic_beanstalk_application.app.name
   solution_stack_name = var.solution_stack
-  version_label       = aws_elastic_beanstalk_application_version.version.name
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "InstanceType"
-    value     = "t3.micro"
-  }
 
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
     value     = "SingleInstance"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t3.micro"
   }
 }
