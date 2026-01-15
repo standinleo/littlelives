0a0334ad07d04e6d65a1722067d560f7cdc5a7ed
standinleo <ne.minhtri@gmail.com>
2026-01-15 08:23:45 +0000

    Edit outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,35 @@
+output "s3_bucket_name" {
+  description = "Name of the uploads S3 bucket"
+  value       = module.s3.bucket_name
+}
+
+output "deployment_bucket_name" {
+  description = "Name of the deployment S3 bucket"
+  value       = module.s3.deployment_bucket_name
+}
+
+output "rds_endpoint" {
+  description = "Endpoint of the RDS instance"
+  value       = module.rds.endpoint
+}
+
+output "elastic_beanstalk_environment_name" {
+  description = "Name of the Elastic Beanstalk environment"
+  value       = module.elastic_beanstalk.environment_name
+}
+
+output "elastic_beanstalk_environment_id" {
+  description = "ID of the Elastic Beanstalk environment"
+  value       = module.elastic_beanstalk.environment_id
+}
+
 output "elastic_beanstalk_url" {
-  value = aws_elastic_beanstalk_environment.env.endpoint_url
+  description = "URL of the Elastic Beanstalk environment"
+  value       = module.elastic_beanstalk.ebs-url
+}
+
+output "cname" {
+  description = "Name of the Elastic Beanstalk cname"
+  value       = module.elastic_beanstalk.cname
+
 }
